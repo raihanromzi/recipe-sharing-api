939a1053a4895b52679ddf753fac1f2a3a9fc1ac
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-10-27 09:44:52 +0700

    setup express and installed package

---
+++ src/utils/response.js
@@ -0,0 +1,17 @@
+const cleanObj = require('./cleanObj');
+
+const responseSuccess = (code, status, data) =>
+  cleanObj({
+    code,
+    status,
+    data: [data],
+  });
+
+const responseError = (code, status, error) =>
+  cleanObj({
+    code,
+    status,
+    errors: error,
+  });
+
+module.exports = { responseSuccess, responseError };
