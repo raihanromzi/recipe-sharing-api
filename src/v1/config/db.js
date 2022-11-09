79715cf854ccf6d22e1b0272559bbb84bd4ca3ac
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-11-09 09:33:45 +0700

    added planetscale db

--- src/v1/config/db.js
+++ src/v1/config/db.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-module.exports = mysql.createConnection(
-  {
-    host: process.env.DB_HOSTNAME,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  } || process.env.DB_URL,
-);
+module.exports = mysql.createConnection({
+  host: process.env.DB_HOSTNAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// module.exports = mysql.createConnection(process.env.DATABASE_URL);
