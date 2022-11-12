399823bd58c73d494fb95f232453bbf7ac73f14b
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-11-12 11:13:57 +0700

    delete db.js localhost

--- src/v1/config/db.js
+++ src/v1/config/db.js
@@ -1,12 +1,5 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-// module.exports = mysql.createConnection({
-//   host: process.env.DB_HOSTNAME,
-//   user: process.env.DB_USERNAME,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
 // Planet Scale
 module.exports = mysql.createConnection(process.env.DATABASE_URL);
